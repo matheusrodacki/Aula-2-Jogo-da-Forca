e877a5925663cfdcc113c8bb944b479a8274a4de
matheusrodacki <matheus.rodacki@outlook.com>
2023-05-31 17:45:23 -0300

    Fix Identation

--- forca.py
+++ forca.py
@@ -1,11 +1,11 @@
 def jogar():
 
-  print("***************************")
-  print("Bem vindo ao jogo de Forca!")
-  print("***************************\n")
+    print("***************************")
+    print("Bem vindo ao jogo de Forca!")
+    print("***************************\n")
 
-  print("Obrigado por jogar!")
+    print("Obrigado por jogar!")
 
 
 if (__name__ == "__main__"):
-  jogar()
+    jogar()
--- jogo.py
+++ jogo.py
@@ -10,8 +10,8 @@ print("(1) Forca (2) Adivinhação")
 jogo = int(input("Qual o jogo?"))
 
 if (jogo == 1):
-  print("Jogando Forca...\n")
-  forca.jogar()
+    print("Jogando Forca...\n")
+    forca.jogar()
 elif (jogo == 2):
-  print("Jogando Adivinhação...\n")
-  adivinhacao.jogar()
+    print("Jogando Adivinhação...\n")
+    adivinhacao.jogar()
