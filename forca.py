36b88c22dc60bc1271515dab44b82c219e2f6281
matheusrodacki <matheus.rodacki@outlook.com>
2023-06-01 18:21:08 -0300

    Game Base

--- forca.py
+++ forca.py
@@ -4,6 +4,34 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("***************************\n")
 
+    palavra_secreta = "BANANA"
+    forca = ["_","_","_","_","_","_"]
+    
+    enforcou = False
+    acertou = False
+
+    print(forca)
+
+    #enquanto(True)
+    while(not enforcou and not acertou):
+        
+        chute = input("Digite uma letra:").upper().strip()
+        print("\n")
+        
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                forca[index] = letra
+                print(forca[index], end=" ")
+
+            else:
+                print(forca[index], end=" ")    
+            index += 1
+        
+
+        print("\n\njogando...")
+        
+
     print("Obrigado por jogar!")
 
 
