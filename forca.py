86b4cd369df058ce048ebca78bd7aaad07d25ecb
matheusrodacki <matheus.rodacki@outlook.com>
2023-06-02 13:00:10 -0300

    split the code into functions

--- forca.py
+++ forca.py
@@ -1,25 +1,17 @@
+import random
+
 def jogar():
-    import random
 
-    print("***************************")
-    print("Bem vindo ao jogo de Forca!")
-    print("***************************\n")
+    inicializa_jogo()
 
-    arquivo = open("palavras.txt", "r")
-    palavras = []
-    
-    for linha in arquivo:
-        palavras.append(linha.strip().upper())        
-    arquivo.close()
-
-    palavra_secreta = palavras[random.randrange(0, len(palavras))]
+    palavra_secreta = carrega_palavra_secreta()
     forca = ["_" for letra in palavra_secreta]
+
+    print(forca)
 
     enforcou = False
     acertou = False
     erros = 0
-
-    print(forca)
 
     # enquanto(True)
     while (not enforcou and not acertou):
@@ -28,19 +20,10 @@ def jogar():
         print("\n")
 
         if (chute in palavra_secreta):
-            index = 0
-            for letra in palavra_secreta:
-                if (chute == letra):
-                    forca[index] = letra
-                    print(forca[index], end=" ")
-                else:
-                    print(forca[index], end=" ")    
-                index += 1
-            print("\n\njogando...")
+            acerta_chute(chute, forca, palavra_secreta)
         else:
             print("Você errou! Tentativas restantes {}".format(5 - erros))
             print(forca)
-            print("")
             erros += 1
 
         enforcou = erros == 6
@@ -52,6 +35,36 @@ def jogar():
         print("Você perdeu :( ")
 
     print("Obrigado por jogar!")
+
+
+def inicializa_jogo():
+    print("***************************")
+    print("Bem vindo ao jogo de Forca!")
+    print("***************************\n")
+
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        palavras.append(linha.strip().upper())
+    arquivo.close()
+
+    palavra_secreta = palavras[random.randrange(0, len(palavras))]
+    return palavra_secreta
+
+
+def acerta_chute(chute, forca, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if (chute == letra):
+            forca[index] = letra
+            print(forca[index], end=" ")
+        else:
+            print(forca[index], end=" ")
+        index += 1
+    print("\n\njogando...")
 
 
 if (__name__ == "__main__"):
